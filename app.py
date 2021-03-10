8705354cb53edebc10005f3e14c01f360cebfd46
Haizhu Hong <hong8890@163.com>
2021-03-10 20:05:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -21,6 +21,11 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 tfidf_rank = pd.read_csv('https://github.com/haizhuhong/capstone-dashboard/raw/main/dashboard_data.csv', low_memory=False)
 unigrams = pd.read_csv('https://github.com/haizhuhong/capstone-dashboard/raw/main/unigram_tfidf_scores_math.csv', low_memory=False)
 unigrams_keywords = pd.read_csv('https://github.com/haizhuhong/capstone-dashboard/raw/main/unigrams_keywords_math.csv', low_memory=False)
+bigrams = pd.read_csv('https://github.com/haizhuhong/capstone-dashboard/raw/main/bigram_tfidf_scores_math.csv', low_memory=False)
+bigrams_keywords = pd.read_csv('https://github.com/haizhuhong/capstone-dashboard/raw/main/bigrams_keywords_math.csv', low_memory=False)
+alldata = pd.read_csv('https://github.com/haizhuhong/capstone-dashboard/raw/main/all_tfidf_math.csv', low_memory=False)
+alldata_keywords = pd.read_csv('https://github.com/haizhuhong/capstone-dashboard/raw/main/all_tfidf_keywords_math.csv', low_memory=False)
+
 
 markdown_text = """
 
@@ -33,10 +38,22 @@ fig_1.layout.width = 1280
 fig_1.layout.height = 650
 
 ### Horizontal Bar Charts
+#Unigrams
 unigrams_top30 = unigrams.sort_values('TF-IDF_Z', ascending=False).head(30)
 unigrams_keywords_top30 = unigrams_keywords.sort_values('TF-IDF_Z', ascending=False).head(30)
-fig_2 = px.bar(unigrams_top30, x='sub_indicator', y='TF-IDF_Z', hover_data=['TF-IDF', 'TF-IDF_Z'])
-fig_3 = px.bar(unigrams_keywords_top30, x='sub_indicator', y='TF-IDF_Z', hover_data=['TF-IDF', 'TF-IDF_Z'])
+fig_2 = px.bar(unigrams_top30, x='TF-IDF_Z', y='sub_indicator', orientation='h', hover_data=['TF-IDF', 'TF-IDF_Z'])
+fig_3 = px.bar(unigrams_keywords_top30, x='TF-IDF_Z', y='sub_indicator', orientation='h',hover_data=['TF-IDF', 'TF-IDF_Z'])
+#Bigrams
+bigrams_top30 = bigrams.sort_values('TF-IDF_Z', ascending=False).head(30)
+bigrams_keywords_top30 = bigrams_keywords.sort_values('TF-IDF_Z', ascending=False).head(30)
+fig_4 = px.bar(bigrams_top30, x='TF-IDF_Z', y='sub_indicator', orientation='h', hover_data=['TF-IDF', 'TF-IDF_Z'])
+fig_5 = px.bar(bigrams_keywords_top30, x='TF-IDF_Z', y='sub_indicator', orientation='h',hover_data=['TF-IDF', 'TF-IDF_Z'])
+#All_data
+alldata_top30 = alldata.sort_values('TF-IDF_Z', ascending=False).head(30)
+alldata_keywords_top30 = alldata_keywords.sort_values('TF-IDF_Z', ascending=False).head(30)
+fig_6 = px.bar(unigrams_top30, x='TF-IDF_Z', y='sub_indicator', orientation='h', hover_data=['TF-IDF', 'TF-IDF_Z'])
+fig_7 = px.bar(unigrams_keywords_top30, x='TF-IDF_Z', y='sub_indicator', orientation='h',hover_data=['TF-IDF', 'TF-IDF_Z'])
+
 ### Create app
 app = Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
@@ -67,6 +84,30 @@ app.layout = html.Div(
             html.H4("Top 30 Unigram Indicators in WWC Math Corpus"),
             
             dcc.Graph(figure=fig_3)], style = {'width':'50%', 'float':'right'}),
+        
+        html.Div([
+            
+            html.H4("Top 30 Bigrams in WWC Math Corpus"),
+            
+            dcc.Graph(figure=fig_4)], style = {'width':'50%', 'float':'left'}),
+        
+        html.Div([
+            
+            html.H4("Top 30 Bigram Indicators in WWC Math Corpus"),
+            
+            dcc.Graph(figure=fig_5)], style = {'width':'50%', 'float':'right'}),
+        
+        html.Div([
+            
+            html.H4("Top 30 N-Grams in WWC Math Corpus"),
+            
+            dcc.Graph(figure=fig_6)], style = {'width':'50%', 'float':'left'}),
+        
+        html.Div([
+            
+            html.H4("Top 30 N-Gram Indicators in WWC Math Corpus"),
+            
+            dcc.Graph(figure=fig_7)], style = {'width':'50%', 'float':'right'}),
         
         
     ]
